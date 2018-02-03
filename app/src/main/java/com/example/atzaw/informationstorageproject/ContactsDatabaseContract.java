7561c098c4529deaded434fe26910c7a95459914
Andy <atzawada@gmail.com>
2018-02-03 15:01:23 -0500

    Implement database contract for contacts.

--- app/src/main/java/com/example/atzaw/informationstorageproject/ContactsDatabaseContract.java
+++ app/src/main/java/com/example/atzaw/informationstorageproject/ContactsDatabaseContract.java
@@ -1,8 +1,24 @@
 package com.example.atzaw.informationstorageproject;
 
+import android.provider.BaseColumns;
+
 /**
  * Created by atzaw on 2/3/2018.
  */
 
 public final class ContactsDatabaseContract {
+
+    private ContactsDatabaseContract() {
+        // Private to prevent instantiation
+    }
+
+    public static class ContactEntry implements BaseColumns {
+        public static final String TABLE_NAME = "contacts";
+        public static final String COLUMN_NAME_FIRSTNAME = "first_name";
+        public static final String COLUMN_NAME_LASTNAME = "last_name";
+        public static final String COLUMN_NAME_NUMBER = "phone_number";
+        public static final String COLUMN_NAME_EMAIL = "email";
+    }
+
+
 }
